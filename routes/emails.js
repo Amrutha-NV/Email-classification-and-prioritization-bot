10138fdbbc1ec93d7fc0162109f2553d0c9b5c32
Amrutha-NV <amruthavasudeva28@gmail.com>
2025-09-19 22:57:27 +0530

    implemented crud operations

--- routes/emails.js
+++ routes/emails.js
@@ -4,8 +4,34 @@ const router = express.Router();
 const Email = require("../models/emails.js");
 const path = require("path");
 router.use(express.urlencoded({ extended: true }));
+
+//to render compose page
+router.get("/compose", (req, res) => {
+    res.render("./email/compose.ejs");
+});
+// to add emails composed to database
+router.post("/", async(req, res) => {
+        const email = req.body.email
+        const mail = new Email(email);
+        await mail.save();
+        res.redirect("/emails/");
+    })
+    //to read emails from database
 router.get("/", async(req, res) => {
     const emails = await Email.find({});
-    res.render("../views/layouts/bolierplate.ejs");
+    res.render("./email/inbox.ejs", { emails });
+});
+// to view indivisual emails
+router.get("/:id", async(req, res) => {
+    let { id } = req.params;
+    const email = await Email.findById(id);
+    res.render("./email/show.ejs", { email });
+});
+// to delete indivisual email
+router.delete("/:id", async(req, res) => {
+    let { id } = req.params;
+    const email = await Email.findByIdAndDelete(id);
+    res.redirect("/emails/");
 })
+
 module.exports = router;
