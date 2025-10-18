0f62b2853be3f0796d519e6fbcc8cb98a6a123e4
Deepthi-M555 <mdeepthi555@gmail.com>
2025-10-18 23:57:14 +0530

    feat(email): add categorized routes and views for Work, Personal, Promotions, and Spam emails

--- routes/emails.js
+++ routes/emails.js
@@ -9,29 +9,62 @@ router.use(express.urlencoded({ extended: true }));
 router.get("/compose", (req, res) => {
     res.render("./email/compose.ejs");
 });
+
 // to add emails composed to database
 router.post("/", async(req, res) => {
         const email = req.body.email
         const mail = new Email(email);
         await mail.save();
         res.redirect("/emails/");
-    })
-    //to read emails from database
+    });
+
+// Category routes
+
+// Work Emails
+router.get("/work", async (req, res) => {
+    const emails = await Email.find({ label: "work" });
+    res.render("./email/work.ejs", { emails });
+});
+
+// Personal Emails
+router.get("/personal", async (req, res) => {
+    const emails = await Email.find({ label: "personal" });
+    res.render("./email/personal.ejs", { emails });
+});
+
+// Promotions Emails
+router.get("/promotions", async (req, res) => {
+    const emails = await Email.find({ label: "promotions" });
+    res.render("./email/promotions.ejs", { emails });
+});
+
+// Spam Emails
+router.get("/spam", async (req, res) => {
+    const emails = await Email.find({ label: "spam" });
+    res.render("./email/spam.ejs", { emails });
+});
+
+//to read emails from database
 router.get("/", async(req, res) => {
     const emails = await Email.find({});
     res.render("./email/inbox.ejs", { emails });
 });
-// to view indivisual emails
+
+// to view individual emails
 router.get("/:id", async(req, res) => {
     let { id } = req.params;
     const email = await Email.findById(id);
     res.render("./email/show.ejs", { email });
 });
-// to delete indivisual email
+
+// to delete individual email
 router.delete("/:id", async(req, res) => {
     let { id } = req.params;
     const email = await Email.findByIdAndDelete(id);
     res.redirect("/emails/");
-})
+});
+
+
+
 
 module.exports = router;
